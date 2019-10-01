9ed404ac3de3196e637db22bb9533cb567133036
Andrés Ignacio Torres <andresitorresm@gmail.com>
2019-10-01 00:35:43 -0400

    Separate lexer literals tests into specific files

---
+++ test/lexer/CharLiteralsSpec.hs
@@ -0,0 +1,57 @@
+module CharLiteralsSpec where
+
+import Test.Hspec
+import Lexer
+
+spec :: Spec
+spec = describe "Lexer" $ do
+
+  it "accepts `|a|` as a valid char literal" $ do
+    let x = "|a|"
+    s <- scanTokens x
+    case s of
+      Just toks -> do
+        let atok = getAbstractToken $ head toks
+        atok `shouldBe` TkChar
+      Nothing ->
+        error "rejected as an invalid token"
+
+  it "accepts `|b|` as a valid char literal" $ do
+    let x = "|b|"
+    s <- scanTokens x
+    case s of
+      Just toks -> do
+        let atok = getAbstractToken $ head toks
+        atok `shouldBe` TkChar
+      Nothing ->
+        error "rejected as an invalid token"
+
+  it "accepts `|\0|` as a valid char literal" $ do
+    let x = "|\0|"
+    s <- scanTokens x
+    case s of
+      Just toks -> do
+        let atok = getAbstractToken $ head toks
+        atok `shouldBe` TkChar
+      Nothing ->
+        error "rejected as an invalid token"
+
+  it "accepts `|\t|` as a valid char literal" $ do
+    let x = "|\t|"
+    s <- scanTokens x
+    case s of
+      Just toks -> do
+        let atok = getAbstractToken $ head toks
+        atok `shouldBe` TkChar
+      Nothing ->
+        error "rejected as an invalid token"
+
+  it "accepts `|\n|` as a valid char literal" $ do
+    let x = "|\n|"
+    s <- scanTokens x
+    case s of
+      Just toks -> do
+        let atok = getAbstractToken $ head toks
+        atok `shouldBe` TkChar
+      Nothing ->
+        error "rejected as an invalid token"
