05c08fe429944ccf50763ae7300061cea589bf85
Andrés Ignacio Torres <andresitorresm@gmail.com>
2019-10-06 15:37:14 -0400

    Print parsed tokens on exec run, or show an improved message

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib
+import Lib (mainFunc)
 
 main :: IO ()
-main = someFunc
+main = mainFunc
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,22 +1,26 @@
 module Lib
-    ( someFunc
+    ( mainFunc
     ) where
 import Lexer (Token (..), AlexUserState(..), alexMonadScan, scanTokens)
+import Parser (parse)
 import System.Environment (getArgs)
 import System.IO (openFile, IOMode(..), hGetContents)
 
-someFunc :: IO ()
-someFunc = do
+mainFunc :: IO ()
+mainFunc = do
     args <- getArgs
-    if null args then
-        putStrLn "Mano qlq"
+    if null args then do
+        putStrLn "\x1b[31m\x1b[1mFireLink\x1b[0m"
+        putStrLn "Your journey must be started from a \x1b[1m\x1b[31m<file path>\x1b[0m, ashen one."
+        putStrLn "Usage: \t stack run <path>"
     else do
         let programFile = head args
         handle <- openFile programFile ReadMode
         contents <- hGetContents handle
         tokens <- scanTokens contents
         case tokens of
-            Just validTokens ->
-                print validTokens
+            Just validTokens -> do
+                let parsedProgram = parse validTokens
+                putStrLn $ show parsedProgram
             Nothing ->
-                putStrLn "Fix your mistakes, ashen one."
+                putStrLn "Fix your lexical mistakes, ashen one."
