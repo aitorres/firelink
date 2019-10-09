ee8da72476e13110b2ebbad7143eaa358e701ac8
German Robayo <germanrobayo33@gmail.com>
2019-10-09 11:38:44 -0400

    add tests for substraction

--- test/Parser/ExpressionsSpec.hs
+++ test/Parser/ExpressionsSpec.hs
@@ -30,3 +30,27 @@ spec = describe "Expressions" $ do
             CodeBlock
                 [InitializedDeclaration Const (Id "patata") BigInt (Add (Add (IntLit 1) (IntLit 2)) (IntLit 3))]
                 _)) -> True)
+    it "accepts `1 - 2` as an expression" $
+        runTestForValidProgram (buildProgramWithExpr "1 - 2")
+        (\(Program _ _ (
+            CodeBlock
+                [InitializedDeclaration Const (Id "patata") BigInt (Substract (IntLit 1) (IntLit 2))]
+                _)) -> True)
+    it "accepts `1 - 2 - 3` as an expressions and associates to the left" $
+        runTestForValidProgram (buildProgramWithExpr "1 - 2 - 3")
+        (\(Program _ _ (
+            CodeBlock
+                [InitializedDeclaration Const (Id "patata") BigInt (Substract (Substract (IntLit 1) (IntLit 2)) (IntLit 3))]
+                _)) -> True)
+    it "accepts `1 - 2 + 3` as an expressions and associates to the left (1 - 2) + 3" $
+        runTestForValidProgram (buildProgramWithExpr "1 - 2 + 3")
+        (\(Program _ _ (
+            CodeBlock
+                [InitializedDeclaration Const (Id "patata") BigInt (Add (Substract (IntLit 1) (IntLit 2)) (IntLit 3))]
+                _)) -> True)
+    it "accepts `1 + 2 - 3` as an expressions and associates to the left (1 + 2) - 3" $
+        runTestForValidProgram (buildProgramWithExpr "1 + 2 - 3")
+        (\(Program _ _ (
+            CodeBlock
+                [InitializedDeclaration Const (Id "patata") BigInt (Substract (Add (IntLit 1) (IntLit 2)) (IntLit 3))]
+                _)) -> True)
