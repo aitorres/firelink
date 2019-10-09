2d6fd047b4cba3039c4ba75e5db0227c4786b0c5
German Robayo <germanrobayo33@gmail.com>
2019-10-08 21:49:21 -0400

    add test for empty declaration block

--- test/Parser/DeclarationSpec.hs
+++ test/Parser/DeclarationSpec.hs
@@ -6,3 +6,10 @@ import Parser
 import Data.Maybe
 import Lexer
 
+spec :: Spec
+spec = describe "Declarations for Block" $
+    it "accepts empty declaration block" $ do
+        let program = buildProgramWithEmptyMain ""
+        tokens <- scanTokens program
+        let ast = extractValidAST tokens
+        ast `shouldSatisfy` (\(Program _ _ (CodeBlock [] _)) -> True)
