18d32f52b70f4675f15052004ae140cb006f64c7
German Robayo <germanrobayo33@gmail.com>
2019-10-13 13:07:27 -0400

    add tests for bounded loops

---
+++ test/Parser/BoundedLoopStructuresSpec.hs
@@ -0,0 +1,32 @@
+module BoundedLoopStructuresSpec where
+
+import Test.Hspec
+import Utils
+import Grammar
+
+buildProgram c = "\
+    \ hello ashen one \
+
+    \ traveling somewhere \
+    \   " ++ c ++ " \
+    \ you died \
+    \ farewell ashen one"
+
+spec :: Spec
+spec = describe "Bounded loop structures" $ do
+    it "rejects bounded loop structures when programmer confuses the id with an expression" $
+        runTestForInvalidProgram $ buildProgram "\
+        \ upgrading |a| with 2 souls until level 123 \
+        \   traveling somewhere \
+        \       with orange saponite say @hello@ \
+        \   you died \
+        \ max level reached"
+    it "accepts well formed bounded iterations" $
+        runTestForValidProgram (buildProgram "\
+        \ upgrading aa with 2 souls until level 123 \
+        \   traveling somewhere \
+        \       with orange saponite say @hello@ \
+        \   you died \
+        \ max level reached") (\(Program _ _ (CodeBlock _ [
+            InstFor (Id "aa") (IntLit 2) (IntLit 123) _
+        ])) -> True)
