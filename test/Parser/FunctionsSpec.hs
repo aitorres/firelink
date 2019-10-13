13d6c938e708a2bd9a7f231765c54f6c2c3d7cbe
German Robayo <germanrobayo33@gmail.com>
2019-10-13 14:00:37 -0400

    add tests for procedures

--- test/Parser/FunctionsSpec.hs
+++ test/Parser/FunctionsSpec.hs
@@ -5,66 +5,126 @@ import Grammar
 import Utils
 
 spec :: Spec
-spec = describe "Functions declarations" $ do
-    let buildProgram c = "\
-    \ hello ashen one \
-    \   " ++ c ++ " \
-    \ traveling somewhere \
-    \   with orange saponite say @@ \
-    \ you died \
-    \ farewell ashen one"
+spec = do
+    describe "Functions declarations" $ do
+        let buildProgram c = "\
+        \ hello ashen one \
+        \   " ++ c ++ " \
+        \ traveling somewhere \
+        \   with orange saponite say @@ \
+        \ you died \
+        \ farewell ashen one"
 
-    it "accepts no-args functions" $
-        runTestForValidProgram (buildProgram "\
-        \ invocation fun \
-        \ with skill of type humanity \
-        \   traveling somewhere \
+        it "accepts no-args functions" $
+            runTestForValidProgram (buildProgram "\
+            \ invocation fun \
+            \ with skill of type humanity \
+            \   traveling somewhere \
+            \   with orange saponite say @@ \
+            \   you died \
+            \ after this return to your world") (\(Program _ [
+                Function (Id "fun") [] BigInt _
+            ] _) -> True)
+        it "accepts 1 val-arg functions" $
+            runTestForValidProgram (buildProgram "\
+            \ invocation fun \
+            \ requesting \
+            \   val a of type sign \
+            \ with skill of type humanity \
+            \   traveling somewhere \
+            \   with orange saponite say @@ \
+            \   you died \
+            \ after this return to your world") (\(Program _ [
+                Function (Id "fun") [
+                    MethodDeclaration Val (Id "a") CharT
+                ] BigInt _
+            ] _) -> True)
+        it "accepts 1 ref-arg functions" $
+            runTestForValidProgram (buildProgram "\
+            \ invocation fun \
+            \ requesting \
+            \   ref a of type sign \
+            \ with skill of type humanity \
+            \   traveling somewhere \
+            \   with orange saponite say @@ \
+            \   you died \
+            \ after this return to your world") (\(Program _ [
+                Function (Id "fun") [
+                    MethodDeclaration Ref (Id "a") CharT
+                ] BigInt _
+            ] _) -> True)
+        it "accepts several args functions" $
+            runTestForValidProgram (buildProgram "\
+            \ invocation fun \
+            \ requesting \
+            \   val a of type sign, \
+            \   ref b of type bonfire \
+            \ with skill of type humanity \
+            \   traveling somewhere \
+            \   with orange saponite say @@ \
+            \   you died \
+            \ after this return to your world") (\(Program _ [
+                Function (Id "fun") [
+                    MethodDeclaration Ref (Id "b") BoolT,
+                    MethodDeclaration Val (Id "a") CharT
+                ] BigInt _
+            ] _) -> True)
+    describe "Procedures" $ do
+        let buildProgram c = "\
+        \ hello ashen one \
+        \   " ++ c ++ " \
+        \ traveling somewhere \
         \   with orange saponite say @@ \
-        \   you died \
-        \ after this return to your world") (\(Program _ [
-            Function (Id "fun") [] BigInt _
-        ] _) -> True)
-    it "accepts 1 val-arg functions" $
-        runTestForValidProgram (buildProgram "\
-        \ invocation fun \
-        \ requesting \
-        \   val a of type sign \
-        \ with skill of type humanity \
-        \   traveling somewhere \
-        \   with orange saponite say @@ \
-        \   you died \
-        \ after this return to your world") (\(Program _ [
-            Function (Id "fun") [
-                MethodDeclaration Val (Id "a") CharT
-            ] BigInt _
-        ] _) -> True)
-    it "accepts 1 ref-arg functions" $
-        runTestForValidProgram (buildProgram "\
-        \ invocation fun \
-        \ requesting \
-        \   ref a of type sign \
-        \ with skill of type humanity \
-        \   traveling somewhere \
-        \   with orange saponite say @@ \
-        \   you died \
-        \ after this return to your world") (\(Program _ [
-            Function (Id "fun") [
-                MethodDeclaration Ref (Id "a") CharT
-            ] BigInt _
-        ] _) -> True)
-    it "accepts several args functions" $
-        runTestForValidProgram (buildProgram "\
-        \ invocation fun \
-        \ requesting \
-        \   val a of type sign, \
-        \   ref b of type bonfire \
-        \ with skill of type humanity \
-        \   traveling somewhere \
-        \   with orange saponite say @@ \
-        \   you died \
-        \ after this return to your world") (\(Program _ [
-            Function (Id "fun") [
-                MethodDeclaration Ref (Id "b") BoolT,
-                MethodDeclaration Val (Id "a") CharT
-            ] BigInt _
-        ] _) -> True)
+        \ you died \
+        \ farewell ashen one"
+
+        it "accepts no-args procedures" $
+            runTestForValidProgram (buildProgram "\
+            \ spell fun \
+            \   traveling somewhere \
+            \   with orange saponite say @@ \
+            \   you died \
+            \ ashen estus flask consumed") (\(Program _ [
+                Procedure (Id "fun") [] _
+            ] _) -> True)
+        it "accepts 1 val-arg procedure" $
+            runTestForValidProgram (buildProgram "\
+            \ spell fun \
+            \ requesting \
+            \   val a of type sign \
+            \   traveling somewhere \
+            \   with orange saponite say @@ \
+            \   you died \
+            \ ashen estus flask consumed") (\(Program _ [
+                Procedure (Id "fun") [
+                    MethodDeclaration Val (Id "a") CharT
+                ] _
+            ] _) -> True)
+        it "accepts 1 ref-arg procedures" $
+            runTestForValidProgram (buildProgram "\
+            \ spell fun \
+            \ requesting \
+            \   ref a of type sign \
+            \   traveling somewhere \
+            \   with orange saponite say @@ \
+            \   you died \
+            \ ashen estus flask consumed") (\(Program _ [
+                Procedure (Id "fun") [
+                    MethodDeclaration Ref (Id "a") CharT
+                ] _
+            ] _) -> True)
+        it "accepts several args procedures" $
+            runTestForValidProgram (buildProgram "\
+            \ spell fun \
+            \ requesting \
+            \   val a of type sign, \
+            \   ref b of type bonfire \
+            \   traveling somewhere \
+            \   with orange saponite say @@ \
+            \   you died \
+            \ ashen estus flask consumed") (\(Program _ [
+                Procedure (Id "fun") [
+                    MethodDeclaration Ref (Id "b") BoolT,
+                    MethodDeclaration Val (Id "a") CharT
+                ] _
+            ] _) -> True)
