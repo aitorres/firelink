a3f451851a1e7d93f02db4476eb046f0df019371
German Robayo <germanrobayo33@gmail.com>
2020-01-18 15:51:35 -0400

    add more tests for `foreach`

--- test/TypeChecking/ForEachInstructionSpec.hs
+++ test/TypeChecking/ForEachInstructionSpec.hs
@@ -3,38 +3,29 @@ module ForEachInstructionSpec where
 import Test.Hspec
 import qualified TestUtils as U
 
+baseProgram :: String -> String
+baseProgram vars = "hello ashen one\n\
+
+\traveling somewhere \n\
+\with\n\
+\   " ++ vars ++ "\n\
+\in your inventory\n\
+\repairing i with titanite from is\n\
+\traveling somewhere\n\
+    \with orange saponite say i\n\
+\you died\n\
+\weaponry repaired\n\
+\you died\n\
+
+\farewell ashen one"
+
 spec :: Spec
 spec =
-    describe "`if` statements" $ do
-        it "should accept a program with boolean guards" $
-            U.shouldNotError "\
-                \hello ashen one\n\
+    describe "`for-each` statements" $ do
+        it "should accept a program with container as an structure and an iterator of the wrapped type" $
+            U.shouldNotError $ baseProgram "var i of type sign,\
+            \var is of type armor of type sign"
 
-                \traveling somewhere \n\
-                \with\n\
-                \   var i of type sign\n\
-                \   var is of type armor of type sign\n\
-                \in your inventory\n\
-                
-                \repairing i with titanite from is\n\
-                \traveling somewhere\n\
-                    \with orange saponite say i\n\
-                \you died\n\
-                \weaponry repaired\n\
-                \you died\n\
-
-                \farewell ashen one"
-
-        it "should reject a program with non-boolean guards" $
-            "hello ashen one\n\
-
-            \traveling somewhere \n\
-            \trust your inventory\n\
-            \   1 + 1:\n\
-            \       traveling somewhere\n\
-            \           with orange saponite say @test@\n\
-            \       you died\n\
-            \inventory closed\n\
-            \you died \
-
-            \farewell ashen one" `U.shouldErrorOn` ("+", 4, 6)
+        it "should reject a program with non-container as an structure and an iterator of the wrapped type" $
+            baseProgram "var i of type sign,\
+            \var is of type sign" `U.shouldErrorOn` ("+", 6, 34)
