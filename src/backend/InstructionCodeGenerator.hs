d49bb74b4f4768f86f90517a8ad5c4799a9172bb
German Robayo <germanrobayo33@gmail.com>
2020-02-04 12:44:25 -0400

    closes #70

--- src/backend/InstructionCodeGenerator.hs
+++ src/backend/InstructionCodeGenerator.hs
@@ -1,9 +1,21 @@
 module InstructionCodeGenerator where
 
 import CodeGenerator
-import ExprCodeGenerator
-import Grammar (Instruction(..))
+import ExprCodeGenerator (genCode')
+import Grammar (Instruction(..), Expr(..), BaseExpr(..), Id(..))
+import TACType
+import Control.Monad.RWS (tell)
 
 instance GenerateCode Instruction where
     genCode (InstPrint expr) = genCode expr
+
+    genCode (InstAsig lvalue@Expr {expAst = IdExpr id} rvalue) = do
+        operand <- genCode' lvalue
+        rValueAddress <- genCode' rvalue
+        tell [ThreeAddressCode
+                { tacOperand = Assign
+                , tacLvalue = Just operand
+                , tacRvalue1 = Just rValueAddress
+                , tacRvalue2 = Nothing
+                }]
 
