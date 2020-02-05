23f9c1e5019f2c92f53fb244fbc8a56098d7d48f
German Robayo <germanrobayo33@gmail.com>
2020-02-05 00:08:46 -0400

    post process: remove unused labels

--- src/backend/BackEndCompiler.hs
+++ src/backend/BackEndCompiler.hs
@@ -5,8 +5,26 @@ import SymTable (Dictionary(..))
 import CodeGenerator (initialState, TAC(..), genCode)
 import InstructionCodeGenerator ()
 import Control.Monad.RWS (runRWST)
+import TACType
 
 backend :: Program -> Dictionary -> IO [TAC]
 backend program dictionary = do
     (_, _, code) <- runRWST (genCode program) dictionary initialState
-    return code
+    return $ removeUnusedLabels code
+
+removeUnusedLabels :: [TAC] -> [TAC]
+removeUnusedLabels tacs = filter removeLabel tacs
+    where
+        usedLabels :: [Int]
+        usedLabels = map getLabelValue $ filter isGoTo tacs
+
+        getLabelValue :: TAC -> Int
+        getLabelValue (ThreeAddressCode GoTo Nothing Nothing (Just (Label label))) = label
+
+        isGoTo :: TAC -> Bool
+        isGoTo (ThreeAddressCode GoTo _ _ _) = True
+        isGoTo _ = False
+
+        removeLabel :: TAC -> Bool
+        removeLabel (ThreeAddressCode NewLabel _ (Just (Label b)) _) = b `elem` usedLabels
+        removeLabel _ = True
