e0fcfb948da424019cc7c04501dc16aa5a963224
Andres Ignacio Torres <andresitorresm@gmail.com>
2020-02-13 23:23:22 -0400

    add tests for function assignments

--- test/TypeChecking/AssignmentInstructionSpec.hs
+++ test/TypeChecking/AssignmentInstructionSpec.hs
@@ -15,6 +15,25 @@ baseProgram c t e = "hello ashen one\n\
 
 \farewell ashen one"
 
+baseFunctionProgram :: String -> String -> String -> String
+baseFunctionProgram fT fR vT = "hello ashen one\n\
+
+\invocation f \n\
+\with skill of type " ++ fT ++ " \n\
+\traveling somewhere\n\
+\    go back with " ++ fR ++ "\n\
+\you died\n\
+\after this return to your world\n\
+
+\traveling somewhere \n\
+\with\n\
+\   var x of type " ++ vT ++ "\n\
+\in your inventory\n\
+\   x <<= summon f\n\
+\you died\n\
+
+\farewell ashen one"
+
 spec :: Spec
 spec =
   describe "`assignments`" $ do
@@ -29,3 +48,12 @@ spec =
 
     it "should reject an assignment to a constant" $
       baseProgram "const" "bonfire <<= lit" "lit" `U.shouldErrorOn` ("<<=", 6, 4)
+
+    it "should accept an assignment of a function that matches type" $
+      U.shouldNotError $ baseFunctionProgram "humanity" "3" "humanity"
+
+    it "should accept an assignment of a function that returns a castable type" $
+      U.shouldNotError $ baseFunctionProgram "small humanity" "3" "big humanity"
+
+    it "should reject an assignment of a function that can't match or cast type" $
+      baseFunctionProgram "hollow" "3.4" "sign" `U.shouldErrorOn` ("<<=", 12, 6)
