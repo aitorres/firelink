7a91648bbcf445e99e4bf030978544bddcb38968
German Robayo <germanrobayo33@gmail.com>
2020-02-21 17:16:43 -0400

    uncomment tests for offset and width

--- test/Semantic/OffsetSpec.hs
+++ test/Semantic/OffsetSpec.hs
@@ -34,9 +34,8 @@ spec :: Spec
 spec =
     describe "Offset calculation" $ do
         it "calculates offset for the first variable in the list" $
-            1 `shouldBe` 1
-        --     testOffset "var x of type humanity" [("x", 0)]
-        -- it "calculates offset for second variable in the list when first variable's type width is multiple of `wordsize`" $
-        --     testOffset "var x of type humanity, var y of type humanity" [("x", 0), ("y", 4)]
-        -- it "calculates offset for second variable in the list when first variable's type width is not a multiple of `wordSize`" $
-        --     testOffset "var x of type sign, var y of type humanity" [("x", 0), ("y", 4)]
+            testOffset "var x of type humanity" [("x", 0)]
+        it "calculates offset for second variable in the list when first variable's type width is multiple of `wordsize`" $
+            testOffset "var x of type humanity, var y of type humanity" [("x", 0), ("y", 4)]
+        it "calculates offset for second variable in the list when first variable's type width is not a multiple of `wordSize`" $
+            testOffset "var x of type sign, var y of type humanity" [("x", 0), ("y", 4)]
