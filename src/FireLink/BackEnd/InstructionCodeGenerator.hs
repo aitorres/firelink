be16493fb264434083ed2aadaf52f232b3ec1436
Andres Ignacio Torres <andresitorresm@gmail.com>
2020-02-25 16:37:06 -0400

    add tac instructions for I/O operations

--- src/FireLink/BackEnd/InstructionCodeGenerator.hs
+++ src/FireLink/BackEnd/InstructionCodeGenerator.hs
@@ -143,6 +143,41 @@ genCodeForInstruction (InstFor id step bound codeblock) next = do
                 stepOp <- genCode' step
                 genOp2Code Add idOp stepOp
 
+{-
+Read statement
+
+Given that we can only read values for scalar types + strings, we simply
+transform the Read statement into the TAC instruction.
+
+?NOTE: Given that we include trilean support, we might have to modify this
+? in order to read trileans as strings and then parse the received value.
+-}
+genCodeForInstruction (InstRead expr) next = do
+    readOperand <- genCode' expr
+    tell [TAC.ThreeAddressCode
+            { TAC.tacOperand = TAC.Read
+            , TAC.tacLvalue = Nothing
+            , TAC.tacRvalue1 = Just readOperand
+            , TAC.tacRvalue2 = Nothing
+            }]
+    genLabel next
+
+{-
+Print statement
+
+Given that we can only print values for scalar types + strings, we simply
+transform the print statement into the TAC instruction.
+-}
+genCodeForInstruction (InstPrint expr) next = do
+    printOperand <- genCode' expr
+    tell [TAC.ThreeAddressCode
+            { TAC.tacOperand = TAC.Print
+            , TAC.tacLvalue = Nothing
+            , TAC.tacRvalue1 = Just printOperand
+            , TAC.tacRvalue2 = Nothing
+            }]
+    genLabel next
+
 -- Aux function for iterations
 setUpIteration :: OperandType -> CodeGenMonad (OperandType, OperandType, OperandType)
 setUpIteration next = do
