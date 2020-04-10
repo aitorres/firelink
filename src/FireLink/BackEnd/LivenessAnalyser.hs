2c1d452d4ebae3e6e9afc7289581b701f312f1c6
German Robayo <germanrobayo33@gmail.com>
2020-04-10 00:42:12 -0400

    remove more code

--- src/FireLink/BackEnd/LivenessAnalyser.hs
+++ src/FireLink/BackEnd/LivenessAnalyser.hs
@@ -12,31 +12,6 @@ import           FireLink.BackEnd.CodeGenerator
 import           FireLink.BackEnd.FlowGraphGenerator
 import           TACType
 
--- | A pair representing an unique point in a program. Its meaning is to hold (blockIds, instructionIndex)
-type ProgramPoint = (Int, Int)
-
--- | A set of live variables at a given time, represented by their unique string name
-type LiveVariables = Set.Set TACSymEntry
-
--- | A map that points a variable to the instruction it's next used in (or none)
-type NextUseMap = Map.Map TACSymEntry (Maybe Int)
-
--- | An object containing a given point's next use information: live variables and next-use information
-data NextUseInfo = NextUseInfo {
-    nuiLiveVars :: !LiveVariables,
-    nuiNextUse :: !NextUseMap
-}
-instance Show NextUseInfo where
-    show NextUseInfo {nuiLiveVars=vs, nuiNextUse=us } =
-        "LiveVars: " ++ show (Set.toList vs) ++ ", NextUse: " ++ show (Map.toList us)
-
--- | Our StateT that will progressively fetch next use info on our basic block
-type NextUseState = StateT NextUseInfo IO
-
--- | A list of pairs of TAC instructions with their respective next use info.
--- | Basically, a basic block (no pun intended) with each instruction tagged with such info.
-type BlockLiveness = [(TAC, NextUseInfo)]
-
 -- | A map that matches variable names to integer representations,
 -- | and a graph that matches such representations' mutual interference
 type InterferenceGraph = (Map.Map TACSymEntry Int, Graph)
