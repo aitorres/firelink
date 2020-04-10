7c9816e0732b95dd53c4367f223622d7a3f44810
German Robayo <germanrobayo33@gmail.com>
2020-04-10 01:37:35 -0400

    add imlpementation for use1 and def1 separately

--- src/FireLink/BackEnd/LivenessAnalyser.hs
+++ src/FireLink/BackEnd/LivenessAnalyser.hs
@@ -24,19 +24,45 @@ getAllVariables = foldr getVariables []
         getVariables :: TAC -> [TACSymEntry] -> [TACSymEntry]
         getVariables (ThreeAddressCode _ a b c) xs = xs ++ catTACSymEntries (catMaybes [a, b, c])
 
+-- | Operations that consists of an assignment of a value to a lvalue
+-- | TODO: Add pointer operations here when their implementation is ready
+assignableOperations :: [Operation]
+assignableOperations = [Assign, Add, Minus, Sub, Mult, Div, Mod, Get, Call, Set]
+
 -- | Calculate variable definitions of a basic block, used by data-flow analysis algorithm for liveness analysis
 -- | Mathematically, def[B] = union of def[n] for n in B.indices
 def :: BasicBlock -> Set.Set TACSymEntry
 def = foldr def' Set.empty
     where
-        assignableOperations :: [Operation]
-        assignableOperations = [Assign, Add, Minus, Sub, Mult, Div, Mod, Get, Call]
-
         def' :: TAC -> Set.Set TACSymEntry -> Set.Set TACSymEntry
-        def' (ThreeAddressCode op (Just (Id v)) _ _) s
-            | op `elem` assignableOperations = Set.insert v s
-            | otherwise = s
-        def' _ s = s
+        def' tac s = s `Set.union` def1 tac
+
+-- | Calculates definition for a single three-address code instruction. Basically, the left side of an instruction
+-- | that assigns a variable.
+def1 :: TAC -> Set.Set TACSymEntry
+def1 :: TAC -> Set.Set TACSymEntry
+def1 (ThreeAddressCode op (Just (Id v)) _ _) s
+    | op `elem` assignableOperations = Set.singleton v
+    | otherwise = Set.empty
+
+-- | Read will end setting a value to its parameter
+def1 (ThreeAddressCode Read _ (Just (Id v)) _) = Set.singleton v
+def1 _ s = Set.empty
+
+-- | Calculate used variables in a basic block prior to any definition of the same variable inside the
+-- | same block
+use :: BasicBlock -> Set.Set TACSymEntry
+use = foldr use' Set.empty
+    where
+        use' :: TAC -> Set.Set TACSymEntry -> Set.Set TACSymEntry
+        use' tac set = error ""
+
+-- | Calculate used variables in a single instruction. That is, their operands
+-- | TODO: review operations on the `otherwise` branch to see if the used values are actually ok
+use1 :: TAC -> Set.Set TACSymEntry
+use1 (ThreeAddressCode op u v w)
+    | op `elem` assignableOperations = Set.fromList $ catTACSymEntries $ catMaybes [v, w]
+    | otherwise = Set.fromList $ catTACSymEntries $ catMaybes [u, v, w]
 
 -- | Given a basic block, generates the interference graph
 -- | by checking all of its instructions, one by one, as an undirected
