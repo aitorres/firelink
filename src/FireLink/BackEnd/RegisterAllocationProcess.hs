ce79fb2ccf6e2caa3213fbe776862057759154d8
German Robayo <germanrobayo33@gmail.com>
2020-04-11 15:49:14 -0400

    add RegisterAllocationProcess.hs

---
+++ src/FireLink/BackEnd/RegisterAllocationProcess.hs
@@ -0,0 +1,21 @@
+{-|
+Module      : FireLink.BackEnd.RegisterAllocationProcess
+Description : Register allocation algorithm by graph coloration
+Stability   : experimental
+
+Attempts to allocate registers by doing chaitan algorithm as described here:
+https://cs.gmu.edu/~white/CS640/p98-chaitin.pdf
+
+[@initialStep@] process all codeblocks as we have infinite registers. Also, on function calls,
+all live variables at that moment are going to be "stored" before them. That is done to
+avoid that the interference graph will have edges between their own variables.
+-}
+module FireLink.BackEnd.RegisterAllocationProcess where
+
+import qualified Data.Map                       as DM
+import           FireLink.BackEnd.CodeGenerator (TACSymEntry (..))
+
+-- Semantic aliases
+type Register = Int
+type Color = Register
+type SymEntryRegisterMap = DM.Map TACSymEntry Color
